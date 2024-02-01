6fd909d2687cffb13f3008965f5a2b7c75613e48
pa38ny <hamid.ayachi@ing.com>
2024-02-01 10:13:48 +0100

    added new branch

---
+++ src/main/java/org/ayachi/techiteasy/TechITeasyApplication.java
@@ -0,0 +1,13 @@
+package org.ayachi.techiteasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechITeasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechITeasyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ayachi/techiteasy/TechITeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ayachi.techiteasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechITeasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
